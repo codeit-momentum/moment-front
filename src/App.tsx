06e2c69223888c67b85f0bfaa8c0fc50a7f0cddb
Gayeon Lee <gayean430@gmail.com>
2025-01-08 03:28:45 +0900

    chore: react query 초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
 import router from './router/Router';
+import queryClient from './apis/queryClient';
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   );
 }
