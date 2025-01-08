db72e899632de35e3d88d16a8bd01dd0b2ff6c02
askjiyun <askjiyun@naver.com>
2025-01-09 00:47:21 +0900

    feat: add global styles and mixin setup

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: apple-system, sans-serif;
+  }
+  ol, ul {
+    list-style: none;
+  }
+
+  a{
+	text-decoration: none;
+	color: inherit;
+	&:hover {
+    	text-decoration: none;
+		color: none;
+	}
+    
+	&:active {
+    	text-decoration: none;
+		color: black;
+	}
+        
+    &:visited {
+    	text-decoration: none;
+		color: black;
+	}
+        
+	&:link {
+    	text-decoration: none;
+		color: black; 
+	}
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background: none;
+  }
+
+  input {
+    outline: none;
+  }
+`;
+
+export default GlobalStyle;
