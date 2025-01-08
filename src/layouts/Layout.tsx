69ce585fa1b93ac748a1b8530bf5208376f02656
P1su <pissssssu@naver.com>
2025-01-08 21:27:58 +0900

    chore: useMobile 훅과 중첩라우팅을 통한 웹앱뷰 적용

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,11 @@
 import useMobile from '../hooks/useMobile';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   useMobile();
 
   return(
-    <></>
+    <Outlet />
   );
 };
 
