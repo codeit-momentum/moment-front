9c8c83c87bf17bb3194e41adf6eb5c463d62546b
askjiyun <askjiyun@naver.com>
2025-01-09 23:01:43 +0900

    code review: GlobalStyle 적용 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,14 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import router from "./router/Router";
 import queryClient from "./apis/queryClient";
-import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <>
-      <GlobalStyle>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </GlobalStyle>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import GlobalStyle from "./styles/GlobalStyle.ts";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
