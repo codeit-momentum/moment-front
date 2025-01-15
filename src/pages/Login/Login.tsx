8556374b29a7429209faac29497f077f7b27a82e
Gayeon Lee <gayean430@gmail.com>
2025-01-15 10:13:26 +0900

    code review: 로그인 상수 분리

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,6 @@
 import * as S from './Login.style';
 import IcKakaoLogin from '../../assets/svg/IcKakaoLogin';
-
-const REST_API_KEY: string = import.meta.env.VITE_REST_API_KEY;
-const REDIRECT_URI: string = import.meta.env.VITE_REDIRECT_URI;
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+import { KAKAO_AUTH_URL } from '../../utils/login';
 
 const Login = () => {
   const handleLogin = () => {
