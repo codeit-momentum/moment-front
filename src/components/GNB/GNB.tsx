8148a9db068b1278622cb870a80e5202b4e991fb
P1su <pissssssu@naver.com>
2025-01-18 08:45:05 +0900

    fix: useNavigate 가 아닌 NavLink를 통한 구현으로 수정

--- src/components/GNB/GNB.tsx
+++ src/components/GNB/GNB.tsx
@@ -1,14 +1,7 @@
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import * as S from './GNB.style';
-import { useState } from 'react';
 
 const GNB = () => {
-  const navigate = useNavigate();
-  const [active, setActive] = useState<string>('home');
-  const handldeActive = (label: string, path: string) => {
-    setActive(label);
-    navigate(path);
-  };
   const navList = [
     { label: 'home', name: '홈', path: '/home', icon: null },
     { label: 'moment', name: '모멘트', path: '/moment', icon: null },
@@ -19,13 +12,14 @@ const GNB = () => {
   return (
     <S.GNBLayout>
       {navList.map((nav) => (
-        <S.NavItem
-          key={nav.label}
-          onClick={() => handldeActive(nav.label, nav.path)}
-        >
-          {active === nav.label ? <S.AciteTab /> : <S.UnActiveTab />}
-          <S.NavLabelSpan>{nav.name}</S.NavLabelSpan>
-        </S.NavItem>
+        <NavLink key={nav.label} to={nav.path}>
+          {({ isActive }) => (
+            <S.NavItem>
+              {isActive ? <S.AciteTab /> : <S.UnActiveTab />}
+              <S.NavLabelSpan>{nav.name}</S.NavLabelSpan>
+            </S.NavItem>
+          )}
+        </NavLink>
       ))}
     </S.GNBLayout>
   );
