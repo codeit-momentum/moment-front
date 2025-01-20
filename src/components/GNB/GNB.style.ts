aa8d17f7435e5e8b8096e96d6f76a50a07124fbc
P1su <pissssssu@naver.com>
2025-01-20 16:00:40 +0900

    style: 네비게이션바 아이콘 적용 및 정렬 수정

--- src/components/GNB/GNB.style.ts
+++ src/components/GNB/GNB.style.ts
@@ -1,29 +1,14 @@
 import styled from 'styled-components';
 
 export const GNBLayout = styled.div`
-  ${({ theme: { mixin } }) => mixin.flexBox({ justify: 'space-between' })};
-  background-color: ${({ theme }) => theme.colors.white};
+  ${({ theme: { mixin } }) =>
+    mixin.flexBox({ justify: 'space-between', align: 'flex-start' })};
+  background-color: ${({ theme }) => theme.colors.black};
   position: fixed;
   width: 33.5rem;
   height: 6rem;
   bottom: 0;
 `;
 export const NavItem = styled.ul`
-  ${({ theme: { mixin } }) => mixin.flexCenter()};
-  width: 3.6rem;
-`;
-
-//추후 아이콘 확정 시 수정하겠습니다.
-export const AciteTab = styled.div`
-  width: 3.6rem;
-  height: 3.6rem;
-  background-color: black;
-`;
-export const UnActiveTab = styled.div`
-  width: 3.6rem;
-  height: 3.6rem;
-  background-color: gray;
-`;
-export const NavLabelSpan = styled.span`
-  font-size: 12px;
+  display: flex;
 `;
