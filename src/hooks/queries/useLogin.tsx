d60b73b073a6e363e9ca8fe4b98d33ede5486eed
Gayeon Lee <gayean430@gmail.com>
2025-01-22 18:47:34 +0900

    feat: 카카오 로그인 api 요청 코드 추가

--- src/hooks/queries/useLogin.tsx
+++ src/hooks/queries/useLogin.tsx
@@ -3,15 +3,32 @@ import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import instance from '../../apis/client';
 
-interface LoginResponse {
+interface UserResponse {
+  user: {
+    id: string;
+    userID: string;
+    email: string;
+    nickname: string;
+    friendCode: string;
+    profileImageUrl: string;
+  };
   accessToken: string;
-  refreshToken: string;
 }
 
-const fetchKakaoLogin = async (code: string): Promise<LoginResponse> => {
-  // API path 백엔드 구현 후 수정 필요!
-  const response = await instance.get(`/member/oauth/kakao?code=${code}`);
-  return response.data;
+const fetchUser = async (token: string): Promise<UserResponse> => {
+  const userResponse = await instance.post('/auth/kakao-login/user', {
+    kakaoAccessToken: token,
+  });
+
+  return userResponse.data;
+};
+
+const fetchKakaoLogin = async (code: string): Promise<UserResponse> => {
+  const response = await instance.post('/auth/kakao-login', {
+    code: code,
+  });
+  const kakaoToken = response.data.access_token;
+  return fetchUser(kakaoToken);
 };
 
 const useLogin = (code: string | null) => {
@@ -22,16 +39,15 @@ const useLogin = (code: string | null) => {
     onSuccess: (data) => {
       // 토큰을 로컬 스토리지에 저장
       localStorage.setItem('accessToken', data.accessToken);
-      localStorage.setItem('refreshToken', data.refreshToken);
 
       // 로그인 성공 시 페이지 완전 리로드
-      window.location.replace('/');
+      window.location.replace('/home');
     },
     onError: (error) => {
       console.error('Login failed:', error);
 
       // 에러 메세지 확인을 위해 navigate 사용
-      navigate('/');
+      navigate('/login');
     },
   });
 
