5e21d946f4d0b3532ab06506630b94767b3e50ad
askjiyun <askjiyun@naver.com>
2025-01-23 13:40:02 +0900

    style : GNB style develop 최신화

--- src/components/GNB/GNB.style.ts
+++ src/components/GNB/GNB.style.ts
@@ -13,18 +13,3 @@ export const NavItem = styled.ul`
   ${({ theme: { mixin } }) => mixin.flexCenter()};
   width: 3.6rem;
 `;
-
-//추후 아이콘 확정 시 수정하겠습니다.
-export const AciteTab = styled.div`
-  width: 3.6rem;
-  height: 3.6rem;
-  background-color: black;
-`;
-export const UnActiveTab = styled.div`
-  width: 3.6rem;
-  height: 3.6rem;
-  background-color: gray;
-`;
-export const NavLabelSpan = styled.span`
-  font-size: 12px;
-`;
