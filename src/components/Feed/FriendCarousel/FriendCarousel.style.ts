5c879ce662c8046096a53899e7484b8bd8163f50
P1su <pissssssu@naver.com>
2025-01-27 01:03:17 +0900

    style: Feed 페이지 레이아웃과 캐러셀 스타일 수정

--- src/components/Feed/FriendCarousel/FriendCarousel.style.ts
+++ src/components/Feed/FriendCarousel/FriendCarousel.style.ts
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 export const FriednCarouselLayout = styled.div`
   max-width: 37.5rem;
-  padding-left: 2rem;
-  padding-bottom: 2rem;
+  padding: 0 2rem 3rem 2rem;
   --slide-height: 19rem;
   --slide-spacing: 1rem;
   --slide-size: 50%;
@@ -15,22 +14,24 @@ export const EmblaContainer = styled.div`
   display: flex;
   flex-direction: row;
   touch-action: pan-y pinch-zoom;
-  gap: 3rem;
+  gap: 2.5rem;
 `;
 export const FriendItem = styled.div`
   ${({ theme: { mixin } }) => mixin.flexCenter()};
-  width: 6.1rem;
+  width: 5.4rem;
+  height: 7rem;
   flex-shrink: 0;
-  gap: 1rem;
+  gap: 0.4rem;
   transform: translate3d(0, 0, 0);
 `;
 export const FriendProfileImage = styled.img<{ $isClicked: boolean }>`
-  width: 6rem;
-  height: 6rem;
+  display: block;
+  width: 5rem;
+  height: 5rem;
   border-radius: 310px;
-  ${({ $isClicked }) =>
+  ${({ theme, $isClicked }) =>
     $isClicked &&
     `
-    border: 2px solid blue;
+    border: 2px solid ${theme.colors.blue};
   `}
 `;
