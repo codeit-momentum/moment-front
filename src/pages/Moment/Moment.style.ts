a9b7ac555a539921af487d82fceb5281196b7d74
Gayeon Lee <gayean430@gmail.com>
2025-01-26 10:34:33 +0900

    style: 모멘트 탭바 스타일 설정

--- src/pages/Moment/Moment.style.ts
+++ src/pages/Moment/Moment.style.ts
@@ -4,5 +4,5 @@ export const MomentLayout = styled.div`
   ${({ theme: { mixin } }) =>
     mixin.flexBox({ direction: 'column', justify: 'flex-start' })};
   width: 100%;
-  gap: 3.8rem;
+  gap: 2rem;
 `;
