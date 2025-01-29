91a5091ef349199bf95ace811f32a20c778bacc6
askjiyun <askjiyun@naver.com>
2025-01-28 21:51:27 -0800

    code review : GNB style 수정전 코드로 변경

--- src/components/GNB/GNB.style.ts
+++ src/components/GNB/GNB.style.ts
@@ -1,15 +1,14 @@
 import styled from 'styled-components';
 
 export const GNBLayout = styled.div`
-  ${({ theme: { mixin } }) => mixin.flexBox({ justify: 'space-between' })};
-  background-color: ${({ theme }) => theme.colors.white};
+  ${({ theme: { mixin } }) =>
+    mixin.flexBox({ justify: 'space-between', align: 'flex-start' })};
+  background-color: ${({ theme }) => theme.colors.black};
   position: fixed;
-  width: 37.5rem;
-  height: 5.7rem;
-  padding: 0 4rem 2rem 4rem;
+  width: 33.5rem;
+  height: 6rem;
   bottom: 0;
 `;
 export const NavItem = styled.ul`
-  ${({ theme: { mixin } }) => mixin.flexCenter()};
-  width: 3.6rem;
+  display: flex;
 `;
