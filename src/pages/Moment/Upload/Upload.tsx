3a3597ea46033a84e4a1502ddb36b6455bd67e03
Gayeon Lee <68602093+gayeooon@users.noreply.github.com>
2025-02-05 08:10:52 +0900

    [모멘트페이지] 모멘트 조회, 인증 API 연동 (#71)
    * feat: 모멘트 상세조회 및 업로드 훅 작성
    
    * feat: useUpload 훅 구현 및 모멘트 업로드 API 연동
    
    * fix: 이미지 업로드 시 File 객체 전송 및 Blob URL 에러 처리 추가
    
    * style: moment 페이지 컴포넌트 정렬 수정 및 type assertion 삭제
    
    * feat: 모멘트 기능 메인 페이지 API 연동

--- src/pages/Moment/Upload/Upload.tsx
+++ src/pages/Moment/Upload/Upload.tsx
@@ -7,48 +7,29 @@ import OKModal from '../../../components/Modal/OKModal/OKModal';
 import Button from '../../../components/Button/Button';
 import IcBack from '../../../assets/svg/IcBack';
 import * as S from './Upload.style';
-import useGetBucketDetail from '../../../hooks/queries/bucketList/useGetBucketDetail';
-import usePatchBucketUpload from '../../../hooks/queries/bucketList/usePatchBucketUpload';
 import useResponseMessage from '../../../hooks/common/useErrorHandler';
-
-const mockData = {
-  moment: {
-    id: Number(new Date()),
-    isComplete: false,
-    title: '뜨개질 10코 뜨기',
-  },
-  bucket: {
-    id: Number(new Date()),
-    isComplete: false,
-    title: '번지점프 하기',
-  },
-};
+import useUpload from '../../../hooks/moment/useUpload';
+import { UploadType } from '../../../types/moment';
 
 type UploadProps = {
-  variant: 'moment' | 'bucket';
+  variant: UploadType;
 };
 
 const Upload = ({ variant }: UploadProps) => {
   const { id } = useParams() as { id: string };
   const [isOpen, openModal, closeModal] = useModal();
   const [image, setImage] = useState<string | null>(null);
-  const { mutate: patchBucketUpload } = usePatchBucketUpload();
+  const [imageFile, setImageFile] = useState<File | null>(null);
   const {
     handleError,
     openModal: openErrorModal,
     renderModal,
   } = useResponseMessage();
   const navigate = useNavigate();
-
-  const { data, isError, isLoading } = useGetBucketDetail(id);
+  const { data, isLoading, isError, patchUpload } = useUpload(variant, id);
 
   useEffect(() => {
-    if (
-      isError ||
-      data?.bucket.isChallenging ||
-      data?.bucket.isCompleted ||
-      data?.bucket.type === 'REPEAT'
-    ) {
+    if (isError) {
       alert('존재하지 않는 페이지입니다.');
       navigate('/moment');
     }
@@ -56,10 +37,10 @@ const Upload = ({ variant }: UploadProps) => {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!image) return;
+    if (!imageFile) return;
 
-    patchBucketUpload(
-      { id, image },
+    patchUpload(
+      { id, imageFile },
       {
         onSuccess: openModal,
         onError: (error) => {
@@ -79,6 +60,7 @@ const Upload = ({ variant }: UploadProps) => {
     }
   };
 
+  // 임시 구현
   if (isLoading || !data) {
     return <div>로딩중 ... </div>;
   }
@@ -90,9 +72,13 @@ const Upload = ({ variant }: UploadProps) => {
           <IcBack />
         </S.BackButton>
       </S.Header>
-      <S.TitleSpan>{data.bucket.content}</S.TitleSpan>
+      <S.TitleSpan>{data.content}</S.TitleSpan>
       <S.ImageUploadLayout onSubmit={handleSubmit}>
-        <ImageUpload image={image} setImage={setImage} />
+        <ImageUpload
+          image={image}
+          setImage={setImage}
+          setImageFile={setImageFile}
+        />
         <Button
           disabled={!image}
           type="submit"
@@ -101,26 +87,20 @@ const Upload = ({ variant }: UploadProps) => {
           인증하기
         </Button>
       </S.ImageUploadLayout>
-      {isOpen &&
-        (variant === 'moment' ? (
-          <Modal>
-            <OKModal
-              title={mockData[variant].title}
-              mainText=" 완료!"
-              subText="새로운 모멘트를 인증했네요"
-              onClose={handleCloseModal}
-            />
-          </Modal>
-        ) : (
-          <Modal>
-            <OKModal
-              title={data.bucket.content}
-              mainText=" 완료!"
-              subText="새로운 버킷리스트를 달성했네요"
-              onClose={handleCloseModal}
-            />
-          </Modal>
-        ))}
+      {isOpen && (
+        <Modal>
+          <OKModal
+            title={data.content}
+            mainText=" 완료!"
+            subText={
+              variant === 'moment'
+                ? '새로운 모멘트를 인증했네요'
+                : '새로운 버킷리스트를 달성했네요'
+            }
+            onClose={handleCloseModal}
+          />
+        </Modal>
+      )}
       {renderModal()}
     </S.UploadLayout>
   );
