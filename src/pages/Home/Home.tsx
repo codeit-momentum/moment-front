88b8a1474689bd40aa4c881b89513673c11531e3
권도희 <145524046+dh2e@users.noreply.github.com>
2025-02-05 02:07:21 +0900

    fix: pages Home 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,20 +4,14 @@ import TodayMomentSection from '../../components/Home/TodayMomentSection/TodayMo
 import Bucketlist from '../../components/Home/Bucketlist/Bucketlist';
 
 const Home = () => {
-  // Mock 데이터: 실제 백엔드 fetch 대체 가능
-  const fetchYear = () => {
-    return new Date().getFullYear(); // 현재 연도 반환
-  };
-
-  const year = fetchYear(); // 연도 가져오기
-
   return (
     <S.HomeLayout>
       <Header />
       <TodayMomentSection />
-      <Bucketlist progress={50} year={year} />
+      <Bucketlist progress={50} />
     </S.HomeLayout>
   );
 };
 
 export default Home;
+
