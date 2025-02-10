235368f0e01010438e956262c3f4444f6d73849c
P1su <pissssssu@naver.com>
2025-02-10 20:31:53 +0900

    refactor: 클립보드 중복 오류 해결

--- src/components/Home/TodayMomentSection/MomentList/MomentList.tsx
+++ src/components/Home/TodayMomentSection/MomentList/MomentList.tsx
@@ -2,44 +2,37 @@ import * as S from './MomentList.style';
 import IcClip from '../../../../assets/svg/home/IcClip';
 import IcClipOff from '../../../../assets/svg/home/IcClipOff';
 import { MomentItemType } from './../../../../types/home/index.d';
+import useGetTodayMoments from '../../../../hooks/queries/home/useGetTodayMoments';
+import formatMomentList from '../../../../utils/formatMomentList';
 
-interface MomentListProps {
-  moments: MomentItemType[];
-}
+const MomentList = () => {
+  const currentDate = new Date().toISOString().split('T')[0];
+  const { data: todayData } = useGetTodayMoments(currentDate);
+  const moments: MomentItemType[] = (todayData?.moments || []).map(
+    (moment) => ({
+      id: moment.momentID, // string으로 매핑
+      title: moment.content,
+      isCompleted: moment.isCompleted,
+    }),
+  );
 
-const MomentList = ({ moments }: MomentListProps) => {
   /*
   const handleMomentClick = (id: string, isCompleted: boolean) => {
     if (isCompleted) return;
     console.log(`Redirect to moment detail page for ID: ${id}`);
   };*/
-  const momentItems =
-    moments.length < 3
-      ? [
-          ...moments,
-          ...new Array(3 - moments.length).fill({
-            id: 'empty',
-            title: '-',
-            isCompleted: false,
-          }),
-        ]
-      : moments;
-
+  const momentItems: MomentItemType[] = formatMomentList(moments);
   return (
     <S.MomentListLayout>
-      {moments.length > 0 ? (
-        momentItems.map(({ id, title, isCompleted }) => (
-          <S.MomentBox key={id} $isCompleted={isCompleted}>
-            <S.IconWrapper>
-              {isCompleted ? <IcClipOff /> : <IcClip />}
-            </S.IconWrapper>
-            {isCompleted && <S.ClearBadgeSpan>CLEAR</S.ClearBadgeSpan>}
-            <S.MomentTitleSpan>{title}</S.MomentTitleSpan>
-          </S.MomentBox>
-        ))
-      ) : (
-        <S.MomentTitleSpan>-</S.MomentTitleSpan>
-      )}
+      {momentItems.map(({ id, title, isCompleted }) => (
+        <S.MomentBox key={id} $isCompleted={isCompleted}>
+          <S.IconWrapper>
+            {isCompleted ? <IcClipOff /> : <IcClip />}
+          </S.IconWrapper>
+          {isCompleted && <S.ClearBadgeSpan>CLEAR</S.ClearBadgeSpan>}
+          <S.MomentTitleSpan>{title}</S.MomentTitleSpan>
+        </S.MomentBox>
+      ))}
     </S.MomentListLayout>
   );
 };
