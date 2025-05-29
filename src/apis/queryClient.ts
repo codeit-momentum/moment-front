02e64529cc8544397ff854f85e2b0c18164c213d
P1su <pissssssu@naver.com>
2025-05-30 01:23:10 +0900

    feat: react-query placeholderData를 설정하여 화면 깜빡임 최소화

--- src/apis/queryClient.ts
+++ src/apis/queryClient.ts
@@ -1,10 +1,11 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient, keepPreviousData } from '@tanstack/react-query';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 1,
       refetchOnWindowFocus: false,
+      placeholderData: keepPreviousData,
     },
   },
 });
