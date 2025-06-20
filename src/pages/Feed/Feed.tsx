1d283c24489826d00a2c2cfeba75699b6df5cfe6
P1su <pissssssu@naver.com>
2025-06-20 22:18:36 +0900

    fix: 충돌 오류 해결

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -20,7 +20,6 @@ const Feed = () => {
       {isOpen && currentFriend !== null && (
         <Modal>
           <FeedModal
-            friendList={friendList}
             currentFriend={currentFriend}
             setCurrentFriend={setCurrentFriend}
             closeModal={closeModal}
